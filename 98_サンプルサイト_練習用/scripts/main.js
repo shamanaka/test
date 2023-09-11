6dae3c986092bbcacf195c02bbdd9153a3a17693
s.hamanaka <s.hamanaka@ntt-dk.jp>
2023-09-11 18:34:47 +0900

    Second commit

---
+++ 98_サンプルサイト_練習用/scripts/main.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const hero = new HeroSlider('.swiper');
+    hero.start();
+
+    const _textAnimation = function (el, inview) {
+        if(inview) {
+            const ta = new TweenTextAnimation(el);
+            ta.animate();
+        }
+    }
+
+    const so = new ScrollObserver('.tween-animate-title', _textAnimation);
+
+    const _inviewAnimation = function (el, inview) {
+        if(inview) {
+            el.classList.add('inview');
+        } else {
+            el.classList.remove('inview');
+        }
+    }
+
+    const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
+});
+
